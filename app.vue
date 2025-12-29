67633010dfb2bc7c432429e8f82c5b8c0115a6c5
Cosik001 <artyomformo@gmail.com>
2025-12-29 17:58:12 +0300

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <Header />
+    <main>
+      <NuxtPage />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Header from '~/components/Header.vue'
+</script>
